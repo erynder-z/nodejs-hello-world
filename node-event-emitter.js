f8028a7e236b1fa1b6d608566b57464dbdd6cdda
erynder-z <stfn.bgr@gmail.com>
2022-10-03 17:08:13 +0200

    Create node event emitter

---
+++ node-event-emitter.js
@@ -0,0 +1,16 @@
+import events from 'events';
+
+const eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+const myEventHandler = function () {
+  console.log('I hear a scream!');
+};
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+setTimeout(() => {
+  eventEmitter.emit('scream');
+}, 3000);
